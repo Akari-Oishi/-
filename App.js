2964719cb82d624f5eb396c2258f456a25e0e44f
大石 <65846922+akari112@users.noreply.github.com>
2024-01-30 15:52:26 +0900

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from "react";
+import "./App.css";
+import SpeechToChatGPT from "./components/SpeechToChatGPT";
+
+function App() {
+  return (
+    <div className="App">
+      <SpeechToChatGPT />
+    </div>
+  );
+}
+
+export default App;
